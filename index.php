55e08c0f06d309d52e491b6165f486c2eddd5e14
artemisia000 <misiapierobon@gmail.com>
2022-01-08 11:40:13 +0100

    foreach

--- index.php
+++ index.php
@@ -65,14 +65,28 @@ $faqs = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Document</title>
 </head>
 <body>
 
-<div class="container">
+<div class="navbar">
     <ul>
-        <li></li>
+        <li>Introduzione</li>
+        <li>Norme sulla privacy</li>
+        <li>Termini di servizio</li>
+        <li>Tecnologie</li>
+        <li>Domande frequenti</li>
     </ul>
+</div>
+<div class="container">
+    <?php
+    foreach ($faqs as $faq) {
+        //var_dump($faq); ?>
+        <h2> <?php echo $faq['question']; ?> </h2>
+        <p> <?php echo $faq['answer']; ?></p>
+    
+     <?php } ?>
 </div>
     
 </body>
